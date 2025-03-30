edd4d742375f2058bcf22335206aaeaea0458825
kader009 <kadermollarasel000@gmail.com>
2025-03-30 18:06:25 +0600

    base api and end api create

---
+++ src/redux/endApi.ts
@@ -0,0 +1,32 @@
+import { baseApi } from './baseApi';
+
+const EduNestApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    signUp: build.mutation({
+      query: (userInfo) => ({
+        url: '/api/v1/user',
+        method: 'POST',
+        body: userInfo,
+      }),
+    }),
+
+    // login route here
+    login: build.mutation({
+      query: (userInfo) => ({
+        url: '/login',
+        method: 'POST',
+        body: userInfo,
+      }),
+    }),
+
+
+
+
+
+  }),
+});
+
+export const {
+  useSignUpMutation,
+  useLoginMutation,
+} = EduNestApi;
--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,8 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import registerReducer from '../features/authentication/registerSlice';
+import loginReducer from '../features/authentication/loginSlice';
 
 export const store = configureStore({
-  reducer: {},
-})
+  reducer: {
+    register: registerReducer,
+    login: loginReducer,
+  },
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
