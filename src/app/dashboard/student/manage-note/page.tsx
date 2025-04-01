acdc216d97a03fa1de1bb6e05c6383b900f99a72
kader009 <kadermollarasel000@gmail.com>
2025-04-01 19:21:06 +0600

    manate note page done

--- src/app/dashboard/student/manage-note/page.tsx
+++ src/app/dashboard/student/manage-note/page.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { Button } from '@/components/ui/button';
 
 const page = () => {
   return (
-    <div>manage note</div>
-  )
-}
+    <div>
+      <div>
+        <h2 className="text-center font-semibold my-6">
+          Manage your notes
+        </h2>
+        <div className="overflow-x-auto w-full">
 
-export default page
+        <Table className=' min-w-[600px] w-full '>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[80px]">No</TableHead>
+              <TableHead>Note name</TableHead>
+              <TableHead>Note description</TableHead>
+              <TableHead>Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">INV001</TableCell>
+              <TableCell>Paid</TableCell>
+              <TableCell>Credit Card</TableCell>
+              <Button
+                type="submit"
+                className="w-24 bg-black hover:bg-gray-900 mx-2 text-white py-2 rounded-lg mt-2"
+              >
+                Update
+              </Button>
+              <Button
+                type="submit"
+                className="w-24 bg-black hover:bg-gray-900 text-white py-2 rounded-lg mt-2"
+              >
+                Delete
+              </Button>
+            </TableRow>
+          </TableBody>
+        </Table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
