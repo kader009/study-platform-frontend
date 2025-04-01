f0f55303ed939c545d112bbe2586420111974854
kader009 <kadermollarasel000@gmail.com>
2025-04-01 19:14:17 +0600

    booked session page done

--- src/app/dashboard/student/booked-session/page.tsx
+++ src/app/dashboard/student/booked-session/page.tsx
@@ -1,5 +1,51 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { Button } from '@/components/ui/button';
+
 const page = () => {
-  return <div>booked session</div>;
+  return (
+    <div>
+      <div>
+        <h2 className="text-center font-semibold my-6">
+          A list of your recent booked session
+        </h2>
+        <div className="overflow-x-auto w-full">
+
+        <Table className=' min-w-[600px] w-full '>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[80px]">No</TableHead>
+              <TableHead>Session id</TableHead>
+              <TableHead>Tutor email</TableHead>
+              <TableHead>Transaction (free/paid)</TableHead>
+              <TableHead>Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">INV001</TableCell>
+              <TableCell>Paid</TableCell>
+              <TableCell>Credit Card</TableCell>
+              <TableCell className="">$250.00</TableCell>
+              <Button
+                type="submit"
+                className="w-24 bg-black hover:bg-gray-900 text-white py-2 rounded-lg mt-2"
+              >
+                View Detail
+              </Button>
+            </TableRow>
+          </TableBody>
+        </Table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
--- src/app/dashboard/student/create-note/page.tsx
+++ src/app/dashboard/student/create-note/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { Button } from '@/components/ui/button';
@@ -6,34 +6,61 @@ import { Label } from '@/components/ui/label';
 import { FormEvent } from 'react';
 
 const page = () => {
-  const handleSubmit = (e:FormEvent) => {
+  const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
     console.log();
   };
   return (
     <div>
       <div className="flex justify-center items-center min-h-screen px-4">
-      <div className="max-w-[800px] w-full mx-auto p-6 bg-white rounded-lg shadow-md">
-        <h2 className="text-center text-2xl font-bold text-black mb-4">Create Your Note</h2>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <div>
-            <Label htmlFor="email" className="mb-2 font-semibold">Email</Label>
-            <Input id="email" type="email"  disabled className="border border-black" />
-          </div>
-          <div>
-            <Label htmlFor="title" className="mb-2 font-semibold">Title</Label>
-            <Input id="title" type="text" placeholder="Enter a title"   className="border border-black" />
-          </div>
-          <div>
-            <Label htmlFor="description" className="mb-2 font-semibold">Description</Label>
-            <Textarea id="description" placeholder="Write your notes here"  className="border border-black" />
-          </div>
-          <Button type="submit" className="w-24 bg-black hover:bg-gray-900 text-white py-2 rounded-lg">Submit</Button>
-        </form>
+        <div className="max-w-[800px] w-full mx-auto p-6 bg-white rounded-lg shadow-md">
+          <h2 className="text-center text-2xl font-bold text-black mb-4">
+            Create Your Note
+          </h2>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <div>
+              <Label htmlFor="email" className="mb-2 font-semibold">
+                Email
+              </Label>
+              <Input
+                id="email"
+                type="email"
+                disabled
+                className="border border-black"
+              />
+            </div>
+            <div>
+              <Label htmlFor="title" className="mb-2 font-semibold">
+                Title
+              </Label>
+              <Input
+                id="title"
+                type="text"
+                placeholder="Enter a title"
+                className="border border-black"
+              />
+            </div>
+            <div>
+              <Label htmlFor="description" className="mb-2 font-semibold">
+                Description
+              </Label>
+              <Textarea
+                id="description"
+                placeholder="Write your notes here"
+                className="border border-black"
+              />
+            </div>
+            <Button
+              type="submit"
+              className="w-24 bg-black hover:bg-gray-900 text-white py-2 rounded-lg"
+            >
+              Submit
+            </Button>
+          </form>
+        </div>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
