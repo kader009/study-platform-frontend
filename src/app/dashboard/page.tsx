e73d0899f0567e53a6c2947f98937e60dc371766
kader009 <kadermollarasel000@gmail.com>
2025-04-02 22:52:25 +0600

    card add in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,25 @@
 
 const page = () => {
   return (
-    <div>dashboard</div>
+    <div>
+      <main className="p-6 grid grid-cols-1 md:grid-cols-3 gap-6">
+          {/* Stats Cards */}
+          <div className="bg-white p-6 rounded-lg shadow-md">
+            <h2 className="text-lg font-semibold">📅 Total Sessions</h2>
+            <p className="text-2xl font-bold">50</p>
+          </div>
+
+          <div className="bg-white p-6 rounded-lg shadow-md">
+            <h2 className="text-lg font-semibold">👥 Total Users</h2>
+            <p className="text-2xl font-bold">120</p>
+          </div>
+
+          <div className="bg-white p-6 rounded-lg shadow-md">
+            <h2 className="text-lg font-semibold">📖 Study Materials</h2>
+            <p className="text-2xl font-bold">30</p>
+          </div>
+        </main>
+    </div>
   )
 }
 
