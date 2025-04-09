c8c511412f270662d64200894a49c0bdcc39aad9
kader009 <kadermollarasel000@gmail.com>
2025-04-09 22:06:30 +0600

    pagination done

--- src/app/session/page.tsx
+++ src/app/session/page.tsx
@@ -1,4 +1,6 @@
+'use client';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 interface SessionProps {
   _id: string;
@@ -13,9 +15,25 @@ interface SessionProps {
   sessionDuration: string;
 }
 
-const page = async () => {
-  const data = await fetch('http://localhost:5000/api/v1/session');
-  const sessions = await data.json();
+const Page = () => {
+  const [sessions, setSession] = useState<SessionProps[]>([]);
+  const [page, setPage] = useState(1);
+  const limit = 7;
+
+  const totalPage = Math.ceil(sessions.length / limit);
+  const startIndex = (page - 1) * limit;
+  const paginatedSession = sessions.slice(startIndex, startIndex + limit);
+
+  const fetchSessions = async () => {
+    const res = await fetch(`http://localhost:5000/api/v1/session`);
+    const data = await res.json();
+    setSession(data);
+  };
+
+  useEffect(() => {
+    fetchSessions();
+  }, [page]);
+
   return (
     <div>
       <div className="container mx-auto py-16 px-8">
@@ -24,7 +42,7 @@ const page = async () => {
           <span className="text-blue-600">Approved</span> Sessions
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {sessions.map((session: SessionProps) => {
+          {paginatedSession.map((session: SessionProps) => {
             const currentTime = new Date();
             const sessionEndTime = new Date(session.classEndDate);
             const isSessionEnded = currentTime > sessionEndTime;
@@ -61,9 +79,29 @@ const page = async () => {
             );
           })}
         </div>
+        {/* Pagination Controls */}
+        <div className="flex justify-center items-center mt-10 space-x-4">
+          <button
+            onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
+            disabled={page === 1}
+            className="px-4 py-2 bg-slate-800 text-white rounded disabled:opacity-50"
+          >
+            Previous
+          </button>
+          <span className="text-lg font-semibold text-blue-600">
+            Page {page} of {totalPage}
+          </span>
+          <button
+            onClick={() => setPage((prev) => Math.min(prev + 1, totalPage))}
+            disabled={page === totalPage}
+            className="px-4 py-2 bg-gradient-to-r from-blue-500 to-indigo-600 text-white rounded disabled:opacity-50"
+          >
+            Next
+          </button>
+        </div>
       </div>
     </div>
   );
 };
 
-export default page;
+export default Page;
