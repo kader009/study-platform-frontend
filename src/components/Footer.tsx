cba876456d19a7ffaa5f325132bd82d63eaa8f4e
kader009 <kadermollarasel000@gmail.com>
2025-04-17 22:56:37 +0600

    side bar gap issue solve

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ function Footer() {
   }, []);
 
   return (
-    <footer className="bg-gray-900 text-gray-300 py-12 mt-16">
+    <footer className="bg-gray-900 text-gray-300 py-12">
       <div className="container mx-auto px-4 grid grid-cols-1 md:grid-cols-3 gap-10">
         {/* About Edunest */}
         <div>
