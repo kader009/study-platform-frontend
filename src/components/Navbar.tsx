d07b3e87510f95e197da90ef50feb16ee4c9ea5c
kader009 <kadermollarasel000@gmail.com>
2025-05-05 10:15:00 +0600

    show and hide button upgrade

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -71,7 +71,7 @@ const Navbar = () => {
                 </Link>
 
                 <button
-                  className="block mt-4 md:inline-block md:mt-0 text-black hover:bg-blue-600 hover:text-white px-3 py-2 rounded font-medium"
+                  className="block mt-4 md:inline-block md:mt-0 text-black hover:bg-blue-600 hover:text-white px-3 py-2 rounded-full font-medium"
                   onClick={handleLogout}
                 >
                   Logout
@@ -94,13 +94,13 @@ const Navbar = () => {
               <>
                 <Link
                   href="/register"
-                  className="block mt-4 md:inline-block md:mt-0 text-black hover:bg-blue-600 hover:text-white px-3 py-2 rounded font-medium"
+                  className="block mt-4 md:inline-block md:mt-0 text-black hover:bg-blue-600 hover:text-white px-3 py-2 rounded-full font-medium"
                 >
                   Register
                 </Link>
                 <Link
                   href="/login"
-                  className="block mt-4 md:inline-block md:mt-0 text-black hover:bg-blue-600 hover:text-white px-3 py-2 rounded font-medium"
+                  className="block mt-4 md:inline-block md:mt-0 text-black hover:bg-blue-600 hover:text-white px-3 py-2 rounded-full font-medium"
                 >
                   Login
                 </Link>
