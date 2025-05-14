de5d488d82f59ba01a6fd4255e65588ffb6ab698
kader009 <kadermollarasel000@gmail.com>
2025-05-14 12:25:13 +0600

    blog post link up with online articles

--- src/components/BlogSection.tsx
+++ src/components/BlogSection.tsx
@@ -9,7 +9,7 @@ const blogPosts = [
     excerpt:
       'Discover the best tips for students to stay focused and make the most out of online sessions.',
     image: '/How to Prepare for Online Tutoring.webp',
-    slug: 'online-tutoring-tips',
+    slug: 'https://tutorcruncher.com/blog/how-to-tutor-online-a-best-practice-guide',
   },
   {
     id: 2,
@@ -17,25 +17,28 @@ const blogPosts = [
     excerpt:
       'Explore the essential qualities of a successful tutor on Edunest platform.',
     image: '/Top 5 Skills Every Tutor Should Have.webp',
-    slug: 'skills-for-tutors',
+    slug: 'https://vnaya.com/blog/5-Skills-That-Online-Tutors-Should-Have%21',
   },
   {
     id: 3,
-    title: 'How Edunest is Changing Online ',
+    title: 'How Edunest is Changing Online Education',
     excerpt:
       'Learn how Edunest is making education more accessible and effective.',
     image: '/How Edunest is Changing Online Learning in Bangladesh.webp',
-    slug: 'edunest-transforming-education',
+    slug: 'https://online.umn.edu/story/how-online-education-shaping-future-workforce',
   },
 ];
 
 export default function BlogSection() {
   return (
-    <section className="py-8">
+    <section className="py-6">
       <div className="container mx-auto px-6">
-        <h2 className="text-3xl font-bold text-center mb-10">
+        <h1 className="text-3xl font-bold text-center mb-2">
           Latest Blogs & Resources
-        </h2>
+        </h1>
+        <p className="text-center text-gray-600 mb-10  text-sm">
+        Discover practical tips, expert insights, and the latest updates on online learning
+</p>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
           {blogPosts.map((post) => (
             <Card
@@ -49,10 +52,10 @@ export default function BlogSection() {
                 height={400}
                 className="w-full object-cover rounded-t-xl"
               />
-              <CardContent className="p-4">
+              <CardContent className="p-4 pt-0">
                 <h3 className="text-xl font-semibold mb-2">{post.title}</h3>
                 <p className="text-sm text-gray-600 mb-4">{post.excerpt}</p>
-                <Link href={`/blog/${post.slug}`} target='_blank'>
+                <Link href={`${post.slug}`} target='_blank'>
                   <span className="text-blue-600 hover:underline font-medium">
                     Read more →
                   </span>
