d4a2321bb502dee75117b231fd3b4acbb97335a8
kader009 <kadermollarasel000@gmail.com>
2025-05-16 11:05:21 +0600

    cursor event added for session button

--- src/app/session/page.tsx
+++ src/app/session/page.tsx
@@ -85,7 +85,7 @@ const Page = () => {
           <button
             onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
             disabled={page === 1}
-            className="px-4 py-2 bg-slate-800 text-white rounded disabled:opacity-50"
+            className="px-4 py-2 bg-slate-800 text-white rounded disabled:opacity-50 cursor-pointer"
           >
             Previous
           </button>
@@ -95,7 +95,7 @@ const Page = () => {
           <button
             onClick={() => setPage((prev) => Math.min(prev + 1, totalPage))}
             disabled={page === totalPage}
-            className="px-4 py-2 bg-gradient-to-r from-blue-500 to-indigo-600 text-white rounded disabled:opacity-50"
+            className="px-4 py-2 bg-gradient-to-r from-blue-500 to-indigo-600 text-white rounded disabled:opacity-50 cursor-pointer"
           >
             Next
           </button>
