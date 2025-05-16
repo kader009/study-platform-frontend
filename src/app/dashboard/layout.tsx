330c45c77c014df02ff3431080ca3d44f7131a65
kader009 <kadermollarasel000@gmail.com>
2025-05-16 10:56:48 +0600

    metadata added for dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,11 @@
 import { ReactNode } from 'react';
 import Sidebar from './_components/SideBar';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'EduNest | Dashboard',
+  description: 'A Learing Platform Dashboard',
+};
 
 const Dashbooardlayout = ({ children }: { children: ReactNode }) => {
   return (
