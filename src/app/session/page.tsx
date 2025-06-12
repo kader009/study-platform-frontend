9279c99bd74961f2aeb2b739214b84e5a5d7a6af
kader009 <kadermollarasel000@gmail.com>
2025-06-12 13:06:28 +0600

    loading sppiner show when data not showing on ui

--- src/app/session/page.tsx
+++ src/app/session/page.tsx
@@ -18,6 +18,7 @@ interface SessionProps {
 const Page = () => {
   const [sessions, setSession] = useState<SessionProps[]>([]);
   const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
   const limit = 7;
 
   const totalPage = Math.ceil(sessions.length / limit);
@@ -25,9 +26,18 @@ const Page = () => {
   const paginatedSession = sessions.slice(startIndex, startIndex + limit);
 
   const fetchSessions = async () => {
-    const res = await fetch(`https://study-platform-backend-drxm.onrender.com/api/v1/session`);
-    const data = await res.json();
-    setSession(data);
+    try {
+      setIsLoading(true);
+      const res = await fetch(
+        `https://study-platform-backend-drxm.onrender.com/api/v1/session`
+      );
+      const data = await res.json();
+      setSession(data);
+    } catch (error) {
+      console.error('Error fetching sessions:', error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -38,68 +48,80 @@ const Page = () => {
     <div>
       <div className="container mx-auto py-16 px-8">
         <h1 className="text-3xl font-semibold mb-2 text-center">
-          {' '}
           <span className="text-blue-600">Approved</span> Sessions
         </h1>
-        <p className='text-center mb-8'>Browse sessions that are reviewed and officially approved</p>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {paginatedSession.map((session: SessionProps) => {
-            const currentTime = new Date();
-            const sessionEndTime = new Date(session.classEndDate);
-            const isSessionEnded = currentTime > sessionEndTime;
+        <p className="text-center mb-8">
+          Browse sessions that are reviewed and officially approved
+        </p>
 
-            return (
-              <div
-                key={session._id}
-                className="bg-white rounded-lg shadow-md p-4 transition transform hover:scale-95"
+        {/* Spinner While Loading */}
+        {isLoading ? (
+          <div className="flex justify-center items-center h-60">
+            <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-600 border-solid"></div>
+          </div>
+        ) : (
+          <>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {paginatedSession.map((session: SessionProps) => {
+                const currentTime = new Date();
+                const sessionEndTime = new Date(session.classEndDate);
+                const isSessionEnded = currentTime > sessionEndTime;
+
+                return (
+                  <div
+                    key={session._id}
+                    className="bg-white rounded-lg shadow-md p-4 transition transform hover:scale-95"
+                  >
+                    <h3 className="text-lg font-semibold text-blue-600 mb-2">
+                      {session.sessionTitle}
+                    </h3>
+                    <p className="text-gray-700 text-sm mb-3">
+                      {session.sessionDescription}
+                    </p>
+                    <div className="flex justify-between items-center">
+                      {isSessionEnded ? (
+                        <span className="bg-slate-800 text-white text-sm font-semibold py-2 px-4 rounded-full">
+                          Closed
+                        </span>
+                      ) : (
+                        <span className="bg-slate-800 text-white text-xs font-semibold py-2 px-4 rounded-full">
+                          Open
+                        </span>
+                      )}
+
+                      <Link href={`/session/${session._id}`}>
+                        <span className="bg-gradient-to-r from-blue-500 to-indigo-600 text-white text-sm font-semibold py-2 px-5 rounded-full transition-all shadow-md hover:from-blue-600 hover:to-indigo-700">
+                          Read More
+                        </span>
+                      </Link>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+
+            {/* Pagination Controls */}
+            <div className="flex justify-center items-center mt-10 space-x-4">
+              <button
+                onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
+                disabled={page === 1}
+                className="px-4 py-2 bg-slate-800 text-white rounded disabled:opacity-50 cursor-pointer"
               >
-                <h3 className="text-lg font-semibold text-blue-600 mb-2">
-                  {session.sessionTitle}
-                </h3>
-                <p className="text-gray-700 text-sm mb-3">
-                  {session.sessionDescription}
-                </p>
-                <div className="flex justify-between items-center">
-                  {isSessionEnded ? (
-                    <span className="bg-slate-800 text-white text-sm font-semibold py-2 px-4 rounded-full">
-                      Closed
-                    </span>
-                  ) : (
-                    <span className="bg-slate-800 text-white text-xs font-semibold py-2 px-4 rounded-full">
-                      Open
-                    </span>
-                  )}
-
-                  <Link href={`/session/${session._id}`}>
-                    <span className="bg-gradient-to-r from-blue-500 to-indigo-600 text-white text-sm font-semibold py-2 px-5 rounded-full transition-all shadow-md hover:from-blue-600 hover:to-indigo-700">
-                      Read More
-                    </span>
-                  </Link>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-        {/* Pagination Controls */}
-        <div className="flex justify-center items-center mt-10 space-x-4">
-          <button
-            onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
-            disabled={page === 1}
-            className="px-4 py-2 bg-slate-800 text-white rounded disabled:opacity-50 cursor-pointer"
-          >
-            Previous
-          </button>
-          <span className="text-lg font-semibold text-blue-600">
-            Page {page} of {totalPage}
-          </span>
-          <button
-            onClick={() => setPage((prev) => Math.min(prev + 1, totalPage))}
-            disabled={page === totalPage}
-            className="px-4 py-2 bg-gradient-to-r from-blue-500 to-indigo-600 text-white rounded disabled:opacity-50 cursor-pointer"
-          >
-            Next
-          </button>
-        </div>
+                Previous
+              </button>
+              <span className="text-lg font-semibold text-blue-600">
+                Page {page} of {totalPage}
+              </span>
+              <button
+                onClick={() => setPage((prev) => Math.min(prev + 1, totalPage))}
+                disabled={page === totalPage}
+                className="px-4 py-2 bg-gradient-to-r from-blue-500 to-indigo-600 text-white rounded disabled:opacity-50 cursor-pointer"
+              >
+                Next
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
