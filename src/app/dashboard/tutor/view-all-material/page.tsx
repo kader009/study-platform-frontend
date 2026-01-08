7ee9caa99e2f133d28982b267bb5e331dfd2fde7
Md Abdul Kader Molla <kadermollarasel000@gmail.com>
2026-01-08 11:36:35 +0600

    tutor material page update

--- src/app/dashboard/tutor/view-all-material/page.tsx
+++ src/app/dashboard/tutor/view-all-material/page.tsx
@@ -17,6 +17,7 @@ import {
 } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { RootState } from '@/redux/store/store';
+import TutorMaterialTableSkeleton from '@/components/skeleton/TutorMaterialTableSkeleton';
 import {
   useDeleteMaterialMutation,
   useGetMaterialByemailQuery,
@@ -91,12 +92,7 @@ const Page = () => {
     }
   };
 
-  if (isLoading)
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <div className="w-16 h-16 border-8 border-dashed rounded-full animate-spin border-blue-600"></div>
-      </div>
-    );
+  if (isLoading) return <TutorMaterialTableSkeleton />;
 
   if (isError)
     return (
