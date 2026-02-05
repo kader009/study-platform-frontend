f17e383e275db9fcc22e187dffecceb5ba96593a
Md Abdul Kader Molla <kadermollarasel000@gmail.com>
2026-02-05 19:44:57 +0600

    remove console log for production

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ protocol: 'https', hostname: '**' }],
   },
+
+  compiler:{
+    removeConsole: process.env.NODE_ENV === 'production',
+  }
 };
 
 export default nextConfig;
